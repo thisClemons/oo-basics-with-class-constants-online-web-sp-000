ea9448c4ec6d45d8d21fe9c431dc9251ba8add12
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-23 22:11:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,8 +14,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
+  # def brand=(brand)
+  #   @brand = brand
+  #   BRANDS << brand
+  # end
 end
